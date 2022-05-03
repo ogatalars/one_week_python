b24eae2cb63884e725362266c8f07a92a99037cd
ogatalars <ogataogara@gmail.com>
2022-05-03 17:08:23 -0300

    variables

---
+++ magicTrickExercise.py
@@ -0,0 +1,17 @@
+#This formula always gonna get 3 in the end 
+secret_num = 0
+secret_num += 1
+secret_num *= 2
+secret_num += 4
+secret_num /= 2
+secret_num - 0
+print(secret_num)
+
+
+# def secret_num(num):
+#     num += 1
+#     num *= 2
+#     num += 4
+#     num /= 2
+#     num - num
+#     return num
