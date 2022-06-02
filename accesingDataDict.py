c25209d521b2746258d2e9b3d258b4b460b1b345
ogatalars <ogataogara@gmail.com>
2022-06-02 16:57:05 -0300

    Introdução a dicionários

---
+++ accesingDataDict.py
@@ -0,0 +1,2 @@
+order = {'cost': 3.5 , 'quantity':12}
+print(order['quantity'])  # 12
---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+# Quase que o equivalente a objetos em JS.
+# Não tem ordem; tem keys e values
+# Não tem indexação; tem chaves
+# Key-value Pairs
+# Não tem index, na verdade os index são arbitrarios e sao chamados keys
+# Dicitionaries, know as associative arrays in some other languages, are INDEXED by keys rather than a numerical index. A dictionary holds key-value pairs. Keys can be any immutable type> numbers, strings, booleans, etc
+# values can be any type, including other dictionaries.
+
+empty_dict = {
+    
+}
+dict() # Empty dict
+order = {'Cost': 3.5, 'Quantity': 2}
+movie = {'title': 'The Matrix', 'year': 1999, 'director': 'Wachowski'}
+# You cannot use a list as a key in a dictionary
